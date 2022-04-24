426204f898bc3f4a3012386282364193f890e069
Kovalev Danil <opana.loog@gmail.com>
2022-04-24 20:45:41 +0300

    cleanup x2

--- auth-service/src/main/java/ua/tunepoint/account/data/mapper/UserMapper.java
+++ auth-service/src/main/java/ua/tunepoint/account/data/mapper/UserMapper.java
@@ -4,17 +4,11 @@ import org.mapstruct.Mapper;
 import ua.tunepoint.account.data.entity.Role;
 import ua.tunepoint.account.data.entity.User;
 import ua.tunepoint.model.response.payload.SignupPayload;
-import ua.tunepoint.model.response.payload.UserBasePayload;
-import ua.tunepoint.model.response.payload.UserFullPayload;
 
 @Mapper(componentModel = "spring")
 public interface UserMapper {
 
     SignupPayload toSignupPayload(User user);
-
-    UserBasePayload toUserBase(User user);
-
-    UserFullPayload toUserFull(User user);
 
     default String mapRole(Role role) {
         return role.getName();
